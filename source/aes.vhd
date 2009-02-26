b8a84b540452f5d6da484b4edfb390fa8ae70f1e
Jevin Sweval <jevinsweval ta gmail tod com>
2009-02-25 19:51:22 -0500

    added some aes types

--- source/aes.vhd
+++ source/aes.vhd
@@ -5,13 +5,21 @@
 -- Version:     1.0  Initial Design Entry
 -- Description: AES package
 
-
 library ieee;
 use ieee.std_logic_1164.all;
 
 package aes is
   
    type byte is std_logic_vector(7 downto 0);
-
+   
+   type index is range 0 to 3;
+   
+   type pntr is record
+      i : index;
+      j : index;
+   end record pointer;
+   
+   type blk is array (index, index) of byte;
+   
 end aes;
 
