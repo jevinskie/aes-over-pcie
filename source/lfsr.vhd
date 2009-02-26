f2f03411e3c64e0589e6787a1822f77115c2d09e
Mentor Graphics <mg22@ecegrid.ecn.purdue.edu>
2009-02-26 14:17:37 -0500

    galois lfsr test

--- source/lfsr.vhd
+++ source/lfsr.vhd
@@ -14,30 +14,39 @@ end lfsr;
 
 architecture behav of lfsr is
    signal cnt, next_cnt : unsigned(3 downto 0);
-   signal near : std_logic;
+   signal near,near2 : std_logic;
+   signal g, next_g : unsigned(3 downto 0);
 begin
    
    process(clk)
    begin
       if (rising_edge(clk)) then
          cnt <= next_cnt;
+         g <= next_g;
       end if;
    end process;
 
-   process(cnt, clr, near)
+   process(cnt, clr, near, g)
    begin
       if (clr='1') then
          next_cnt <= (others => '1');
+         next_g <= (others => '1');
       else
          -- next_cnt <= cnt rol 1;
          --next_cnt(3) <= cnt(2) xor cnt(0) xor near;
          --next_cnt(2) <= cnt(1) xor cnt(0);
          next_cnt <= cnt ror 1;
          next_cnt(3) <= cnt(3) xor cnt(0) xor near;
+         
+         next_g(3) <= g(0);
+         next_g(2) <= g(0) xor g(3);
+         next_g(1 downto 0) <= g(2 downto 1);
+         
       end if;
    end process;
    
    near <= '1' when (cnt(3)='0' and cnt(2)='0' and cnt(1)='0') else '0';
+   near2 <= '1' when (g(3)='0' and g(2)='0' and g(1)='0') else '0';
    
    count <= cnt;
    
