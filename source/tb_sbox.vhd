5616644064ff429f11a12f6fda23483a7c065777
Jevin Sweval <jevinsweval ta gmail tod com>
2009-02-27 18:27:48 -0500

    better sbox test bench

--- source/tb_sbox.vhd
+++ source/tb_sbox.vhd
@@ -19,23 +19,34 @@ end tb_sbox;
 
 architecture test of tb_sbox is
    
-   component sbox is
-      port (
-         clk   : in std_logic;
-         a     : in byte;
-         b     : out byte
-      );
-   end component sbox;
+   --component sbox is
+   --   port (
+   --      clk   : in std_logic;
+   --      a     : in byte;
+   --      b     : out byte
+   --   );
+   --end component sbox;
    
-   signal clk  : std_logic := '0';
-   signal a, b : byte;
+   signal clk              : std_logic := '0';
+   signal lut_a, lut_b     : byte;
+   signal data_a, data_b   : byte;
+   signal pipe_a, pipe_b   : byte;
    
    signal stop : std_logic := '1';
    signal gold : byte;
+   
 begin
    
-   dut : sbox port map (
-      clk => clk, a => a, b => b
+   lut : entity work.sbox(lut) port map (
+      clk => clk, a => lut_a, b => lut_b
+   );
+   
+   data : entity work.sbox(dataflow) port map (
+      clk => clk, a => data_a, b => data_b
+   );
+   
+   pipe : entity work.sbox(pipelined) port map (
+      clk => clk, a => pipe_a, b => pipe_b
    );
    
    -- clock when stop isnt asserted
@@ -44,18 +55,29 @@ begin
 process
 begin
    
+   for i in 0 to 255 loop
+      lut_a  <= to_unsigned(i, 8);
+      data_a <= to_unsigned(i, 8);
+      
+      wait for 10 ns;
+      
+      gold         <= work.aes.sbox(i);
+      assert lut_b  = work.aes.sbox(i);
+      assert data_b = work.aes.sbox(i);
+   end loop;
+   
    stop <= '0';
    
    for i in 0 to 256 loop
       if (i <= 255) then
-         a <= to_unsigned(i, 8);
+         pipe_a <= to_unsigned(i, 8);
       end if;
       
       wait for clk_per;
       
       if (i >= 1) then
-         gold <= work.aes.sbox(i-1);
-         assert b = work.aes.sbox(i-1);
+         gold         <= work.aes.sbox(i-1);
+         assert pipe_b = work.aes.sbox(i-1);
       end if;
    end loop;
    
