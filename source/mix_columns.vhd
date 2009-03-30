5b2e5854d8dfada0f5f58bd263f21739ecfb12d4
matt swanson <swanson at purdue dot com>
2009-03-30 17:07:43 -0400

    added Galois field correction to mix_column

--- source/mix_columns.vhd
+++ source/mix_columns.vhd
@@ -33,23 +33,27 @@ architecture dataflow of mix_columns is
    -- r0 = 2a0 + a3 + a2 + 3a1
    -- r1 = 2a1 + a0 + a3 + 3a2
    -- r2 = 2a2 + a1 + a0 + 3a3
-   -- r3 = 2a3 + a2 + a1 + 3a0
-   
-   signal b : col;
+   -- r3 = 2a3 + a2 + a1 + 3a0   
 
-begin
+begin   
+   process(d_in)      
+     variable b : col;   --temp calculation variable
+      begin
+      --multiply by 2 is done with a left shift   
+      --need Galois field correction for b here; i.e. b(i) must be 8-bits still
+      --Algo: check if upper nibble of d_in(1) = 0x80, if so b(i) = b(i) XOR 0x1b
+      for i in 0 to 3 loop
+         b(i) := d_in(i) sll 1;
+         if std_match(d_in(i), "1000----") then
+            b(i) := (b(i) xor x"1b");
+         end if;
+      end loop;
    
-   --multiply by 2 is done with a left shift
-   b(0) <= d_in(0) sll 1;
-   b(1) <= d_in(1) sll 1;
-   b(2) <= d_in(2) sll 1;
-   b(3) <= d_in(3) sll 1;
-   
-   --when multiply by 3 is needed, we can break that into x*(2x)
-   d_out(0) <= b(0) xor d_in(3) xor d_in(2) xor b(1) xor d_in(1); --8e
-   d_out(1) <= b(1) xor d_in(0) xor d_in(3) xor b(2) xor d_in(2); --4d
-   d_out(2) <= b(2) xor d_in(1) xor d_in(0) xor b(3) xor d_in(3); --a1 
-   d_out(3) <= b(3) xor d_in(2) xor d_in(1) xor b(0) xor d_in(0); --bc
-   
+      --when multiply by 3 is needed, we can break that into x*(2x)
+      d_out(0) <= b(0) xor d_in(3) xor d_in(2) xor b(1) xor d_in(1);
+      d_out(1) <= b(1) xor d_in(0) xor d_in(3) xor b(2) xor d_in(2);
+      d_out(2) <= b(2) xor d_in(1) xor d_in(0) xor b(3) xor d_in(3); 
+      d_out(3) <= b(3) xor d_in(2) xor d_in(1) xor b(0) xor d_in(0);
+   end process;
 end architecture dataflow;
 
