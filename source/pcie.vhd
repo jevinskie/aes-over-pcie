b0bbcb49798bfb464b83d7870706c9b9468b7df2
Jevin Sweval <jevinsweval ta gmail tod com>
2009-04-24 17:05:53 -0400

    moved crc functions to pcie package

--- source/pcie.vhd
+++ source/pcie.vhd
@@ -5,6 +5,8 @@
 -- Version:     1.0  Initial Design Entry
 -- Description: PCIe package
 
+use work.aes.all;
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -33,7 +35,99 @@ package pcie is
    attribute enum_encoding of symbol_type : type is
       "01111100";
    
+   function crc_gen(data : byte; crc : word) return word;
+   function lcrc_gen(data : byte; lcrc : dword) return dword;
+   
 end pcie;
 
 package body pcie is
+   
+   
+   function crc_gen (
+      data  : byte;
+      crc   : word
+   ) return word is
+      
+      variable d        : byte;
+      variable c        : word;
+      variable newcrc   : word;
+      
+   begin
+      
+      d := data;
+      c := crc;
+      
+      newcrc(0) := d(6) xor d(3) xor d(0) xor c(8) xor c(11) xor c(14);
+      newcrc(1) := d(7) xor d(6) xor d(4) xor d(3) xor d(1) xor d(0) xor c(8) xor c(9) xor c(11) xor c(12) xor c(14) xor c(15);
+      newcrc(2) := d(7) xor d(6) xor d(5) xor d(4) xor d(3) xor d(2) xor d(1) xor d(0) xor c(8) xor c(9) xor c(10) xor c(11) xor c(12) xor c(13) xor c(14) xor c(15);
+      newcrc(3) := d(7) xor d(6) xor d(5) xor d(4) xor d(3) xor d(2) xor d(1) xor c(9) xor c(10) xor c(11) xor c(12) xor c(13) xor c(14) xor c(15);
+      newcrc(4) := d(7) xor d(5) xor d(4) xor d(2) xor d(0) xor c(8) xor c(10) xor c(12) xor c(13) xor c(15);
+      newcrc(5) := d(6) xor d(5) xor d(3) xor d(1) xor c(9) xor c(11) xor c(13) xor c(14);
+      newcrc(6) := d(7) xor d(6) xor d(4) xor d(2) xor c(10) xor c(12) xor c(14) xor c(15);
+      newcrc(7) := d(7) xor d(5) xor d(3) xor c(11) xor c(13) xor c(15);
+      newcrc(8) := d(6) xor d(4) xor c(0) xor c(12) xor c(14);
+      newcrc(9) := d(7) xor d(5) xor c(1) xor c(13) xor c(15);
+      newcrc(10) := d(6) xor c(2) xor c(14);
+      newcrc(11) := d(7) xor c(3) xor c(15);
+      newcrc(12) := c(4);
+      newcrc(13) := d(6) xor d(3) xor d(0) xor c(5) xor c(8) xor c(11) xor c(14);
+      newcrc(14) := d(7) xor d(4) xor d(1) xor c(6) xor c(9) xor c(12) xor c(15);
+      newcrc(15) := d(5) xor d(2) xor c(7) xor c(10) xor c(13);
+      
+      return newcrc;
+   end crc_gen;
+   
+   
+   function lcrc_gen (
+      data  : byte;
+      lcrc   : dword
+   ) return dword is
+      
+      variable d        : byte;
+      variable c        : dword;
+      variable newcrc   : dword;
+      
+   begin
+      
+      d := data;
+      c := lcrc;
+      
+      newcrc(0) := d(6) xor d(0) xor c(24) xor c(30);
+      newcrc(1) := d(7) xor d(6) xor d(1) xor d(0) xor c(24) xor c(25) xor c(30) xor c(31);
+      newcrc(2) := d(7) xor d(6) xor d(2) xor d(1) xor d(0) xor c(24) xor c(25) xor c(26) xor c(30) xor c(31);
+      newcrc(3) := d(7) xor d(3) xor d(2) xor d(1) xor c(25) xor c(26) xor c(27) xor c(31);
+      newcrc(4) := d(6) xor d(4) xor d(3) xor d(2) xor d(0) xor c(24) xor c(26) xor c(27) xor c(28) xor c(30);
+      newcrc(5) := d(7) xor d(6) xor d(5) xor d(4) xor d(3) xor d(1) xor d(0) xor c(24) xor c(25) xor c(27) xor c(28) xor c(29) xor c(30) xor c(31);
+      newcrc(6) := d(7) xor d(6) xor d(5) xor d(4) xor d(2) xor d(1) xor c(25) xor c(26) xor c(28) xor c(29) xor c(30) xor c(31);
+      newcrc(7) := d(7) xor d(5) xor d(3) xor d(2) xor d(0) xor c(24) xor c(26) xor c(27) xor c(29) xor c(31);
+      newcrc(8) := d(4) xor d(3) xor d(1) xor d(0) xor c(0) xor c(24) xor c(25) xor c(27) xor c(28);
+      newcrc(9) := d(5) xor d(4) xor d(2) xor d(1) xor c(1) xor c(25) xor c(26) xor c(28) xor c(29);
+      newcrc(10) := d(5) xor d(3) xor d(2) xor d(0) xor c(2) xor c(24) xor c(26) xor c(27) xor c(29);
+      newcrc(11) := d(4) xor d(3) xor d(1) xor d(0) xor c(3) xor c(24) xor c(25) xor c(27) xor c(28);
+      newcrc(12) := d(6) xor d(5) xor d(4) xor d(2) xor d(1) xor d(0) xor c(4) xor c(24) xor c(25) xor c(26) xor c(28) xor c(29) xor c(30);
+      newcrc(13) := d(7) xor d(6) xor d(5) xor d(3) xor d(2) xor d(1) xor c(5) xor c(25) xor c(26) xor c(27) xor c(29) xor c(30) xor c(31);
+      newcrc(14) := d(7) xor d(6) xor d(4) xor d(3) xor d(2) xor c(6) xor c(26) xor c(27) xor c(28) xor c(30) xor c(31);
+      newcrc(15) := d(7) xor d(5) xor d(4) xor d(3) xor c(7) xor c(27) xor c(28) xor c(29) xor c(31);
+      newcrc(16) := d(5) xor d(4) xor d(0) xor c(8) xor c(24) xor c(28) xor c(29);
+      newcrc(17) := d(6) xor d(5) xor d(1) xor c(9) xor c(25) xor c(29) xor c(30);
+      newcrc(18) := d(7) xor d(6) xor d(2) xor c(10) xor c(26) xor c(30) xor c(31);
+      newcrc(19) := d(7) xor d(3) xor c(11) xor c(27) xor c(31);
+      newcrc(20) := d(4) xor c(12) xor c(28);
+      newcrc(21) := d(5) xor c(13) xor c(29);
+      newcrc(22) := d(0) xor c(14) xor c(24);
+      newcrc(23) := d(6) xor d(1) xor d(0) xor c(15) xor c(24) xor c(25) xor c(30);
+      newcrc(24) := d(7) xor d(2) xor d(1) xor c(16) xor c(25) xor c(26) xor c(31);
+      newcrc(25) := d(3) xor d(2) xor c(17) xor c(26) xor c(27);
+      newcrc(26) := d(6) xor d(4) xor d(3) xor d(0) xor c(18) xor c(24) xor c(27) xor c(28) xor c(30);
+      newcrc(27) := d(7) xor d(5) xor d(4) xor d(1) xor c(19) xor c(25) xor c(28) xor c(29) xor c(31);
+      newcrc(28) := d(6) xor d(5) xor d(2) xor c(20) xor c(26) xor c(29) xor c(30);
+      newcrc(29) := d(7) xor d(6) xor d(3) xor c(21) xor c(27) xor c(30) xor c(31);
+      newcrc(30) := d(7) xor d(4) xor c(22) xor c(28) xor c(31);
+      newcrc(31) := d(5) xor c(23) xor c(29);
+      
+      return newcrc;
+   end lcrc_gen;
+   
+   
 end pcie;
+
